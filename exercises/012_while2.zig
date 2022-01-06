40c7606102a33e7c95f8dda007bae5c5c82d9720
saggit <andtaotao@qq.com>
2022-01-06 16:32:04 +0800

    (fix): solve exercises 12 to 17

--- exercises/012_while2.zig
+++ exercises/012_while2.zig
@@ -25,9 +25,12 @@ pub fn main() void {
 
     // Please set the continue expression so that we get the desired
     // results in the print statement below.
-    while (n < 1000) : ??? {
+    while (n < 1000) : (n*=2) {
         // Print the current number
         std.debug.print("{} ", .{n});
+        if (n > 1024) {
+           continue;
+        }
     }
 
     // As in the last exercise, we want this to result in "n=1024"
