998c8fa4b466c314a2183c5b4a31ddf71d804363
saggit <andtaotao@qq.com>
2022-01-07 14:33:13 +0800

    (fix): solve exercise 25 to 40

--- exercises/038_structs2.zig
+++ exercises/038_structs2.zig
@@ -40,6 +40,12 @@ pub fn main() void {
     //     health     100
     //     experience 20
     //
+    chars[1] = Character{
+         .class = Class.bard,
+         .gold = 10,
+         .health = 100,
+         .experience = 20,
+     };
     // Feel free to run this program without adding Zump. What does
     // it do and why?
 
